70c53a908cb34cc4ea769d9fbfbc7f7f8283d10b
Maarten Peels <maartenpeels@gmail.com>
2020-12-02 22:05:26 +0100

    Day 2 part 2

--- day2/main.py
+++ day2/main.py
@@ -5,9 +5,7 @@ filename = "./passwords.txt"
 with open(filename) as f:
     lines = [line.rstrip() for line in f]
 
-# Part 1
-valid_passwords = 0
-for line in lines:
+def parse_line(line):
     line_parts = [part.strip() for part in line.split(':')]
     password = line_parts[1]
 
@@ -18,6 +16,13 @@ for line in lines:
     max_amount = int(policy_parts[0].split('-')[1])
     letter = policy_parts[1]
 
+    return min_amount, max_amount, letter, password
+
+# Part 1
+valid_passwords = 0
+for line in lines:
+    min_amount, max_amount, letter, password = parse_line(line)
+
     occ = defaultdict(lambda: 0) 
     for char in password:
         occ[char] += 1
@@ -25,6 +30,18 @@ for line in lines:
     if occ[letter] >= min_amount and occ[letter] <= max_amount:
         valid_passwords += 1
 
-print(f'Valid passwords: {valid_passwords}')
+print(f'Valid passwords part 1: {valid_passwords}')
 
 # Part 2
+valid_passwords = 0
+for line in lines:
+    pos_1, pos_2, letter, password = parse_line(line)
+
+    matches = 0
+    matches += 1 if password[pos_1-1] == letter else 0
+    matches += 1 if password[pos_2-1] == letter else 0
+
+    if matches == 1:
+        valid_passwords += 1
+
+print(f'Valid passwords part 2: {valid_passwords}')
