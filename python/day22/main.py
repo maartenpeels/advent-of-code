f92a30b24c8596b2bfd255a5eaee5e20f5948201
Maarten Peels <maarten.peels@merapar.com>
2020-12-23 12:22:18 +0100

    Day 22 Part 2

--- python/day22/main.py
+++ python/day22/main.py
@@ -12,22 +12,29 @@ def main():
         if player == 2:
             p2 = cards
 
+    part1_answer = part1(p1.copy(), p2.copy())
+    part2_answer = part2(p1.copy(), p2.copy())
+
+    print(f'Answer for part 1: {part1_answer}')
+    print(f'Answer for part 2: {part2_answer}')
+
+
+def part1(p1, p2):
     while len(p1) != 0 and len(p2) != 0:
         play_round(p1, p2)
 
     if len(p1) == 0:
-        part1 = calculate_score(p2)
+        answer = calculate_score(p2)
     else:
-        part1 = calculate_score(p1)
+        answer = calculate_score(p1)
 
-    print(f'Answer for part 1: {part1}')
+    return answer
 
 
-def calculate_score(cards):
-    total = 0
-    for i, card in enumerate(reversed(cards)):
-        total += (i+1) * card
-    return total
+def part2(p1, p2):
+    cards = recursive_combat(p1, p2, set())[1]
+
+    return calculate_score(cards)
 
 
 def play_round(p1, p2):
@@ -40,6 +47,36 @@ def play_round(p1, p2):
     elif p1_card < p2_card:
         p2.append(p2_card)
         p2.append(p1_card)
+
+
+def recursive_combat(p1, p2, history):
+    while len(p1) != 0 and len(p2) != 0:
+        if (tuple(p1), tuple(p2)) in history:
+            return 1, p1
+
+        history.add((tuple(p1), tuple(p2)))
+
+        p1_card = p1.pop(0)
+        p2_card = p2.pop(0)
+
+        if len(p1) >= p1_card and len(p2) >= p2_card:
+            winner, _ = recursive_combat(p1[:p1_card], p2[:p2_card], set())
+        else:
+            winner = 1 if p1_card > p2_card else 2
+
+        if winner == 1:
+            p1.extend([p1_card, p2_card])
+        elif winner == 2:
+            p2.extend([p2_card, p1_card])
+
+    return (1, p1) if len(p1) > 0 else (2, p2)
+
+
+def calculate_score(cards):
+    total = 0
+    for i, card in enumerate(reversed(cards)):
+        total += (i + 1) * card
+    return total
 
 
 if __name__ == '__main__':
