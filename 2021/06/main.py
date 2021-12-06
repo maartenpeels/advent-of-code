afb4937750e39c8d3d19aee4e6d9dcc1eec3dc29
maarten peels <maartenpeels@gmail.com>
2021-12-06 16:33:49 +0100

    Day 6 - Part 2 (Switched to dynamic programming solution)

--- 2021/06/main.py
+++ 2021/06/main.py
@@ -1,10 +1,13 @@
-state = [int(num) for num in open('06/input.txt').readline().strip().split(',')]
-days = 80
+from collections import defaultdict
+fish = [int(num) for num in open('06/input.txt').readline().strip().split(',')]
 
-for day in range(days):
-  for i in range(len(state)):
-    state[i] = state[i] - 1
-    if state[i] < 0:
-      state[i] = 6
-      state.append(8)
-print('Part 1:', len(state))
+def run(state, days):
+  born_at = defaultdict(lambda: 0)
+  for i in state:
+    born_at[i] += 1
+  for day in range(days):
+    born_at[day] = born_at[day] + born_at[day - 7] + born_at[day - 9]
+  return sum(born_at.values()) + len(state)
+
+print('Part 1:', run(fish, 80))
+print('Part 2:', run(fish, 256))
