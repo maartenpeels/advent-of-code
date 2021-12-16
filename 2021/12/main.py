f03c47d7d4bed21a5b9b7215a123974f4ca12553
maarten peels <maartenpeels@gmail.com>
2021-12-16 16:37:15 +0100

    Day 12 Part 2

--- 2021/12/main.py
+++ 2021/12/main.py
@@ -9,9 +9,9 @@ for line in lines:
     connections[right].append(left)
 
 
-def count_routes(_connections):
+def count_routes(_connections, visit_one_small_twice):
     count = 0
-    routes = deque([('start', {'start'}, False)])
+    routes = deque([('start', ['start'], False)])
     while routes:
         location, seen, visited = routes.popleft()
         if location == 'end':
@@ -20,11 +20,14 @@ def count_routes(_connections):
 
         for location in _connections[location]:
             if location not in seen:
-                seen_temp = set(seen)
+                seen_temp = seen.copy()
                 if location.islower():
-                    seen_temp.add(location)
+                    seen_temp.append(location)
                 routes.append((location, seen_temp, visited))
+            elif location in seen and location not in ['start', 'end'] and not visited and visit_one_small_twice:
+                routes.append((location, seen, True))
     return count
 
 
-print('Part 1: ', count_routes(connections))
+print('Part 1: ', count_routes(connections, False))
+print('Part 2: ', count_routes(connections, True))
